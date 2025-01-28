dae5db32fa57dbeb299a3a909f03a8d820a2305b
Srinivasrao28 <srinivasraosrirangam28@gmail.com>
2025-01-28 11:59:52 +0530

    Js Changed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const price = 19.5; // The price of the item
+// const price = document.getElementById("price").value; // The price of the item
 const cid = [
-  ["PENNY", 0.5],
-  ["NICKEL", 0],
-  ["DIME", 0],
-  ["QUARTER", 0],
-  ["ONE", 0],
-  ["FIVE", 0],
-  ["TEN", 0],
-  ["TWENTY", 0],
-  ["ONE HUNDRED", 0],
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
 ];
 
 // Denomination values
